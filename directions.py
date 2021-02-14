13a0e9aa5303404863d0065ebacc8bc9a3203aa2
GitarrenLia <lila.77@freenet.de>
2021-02-14 13:54:25 +0100

    added changing order of files with up down buttons

---
+++ directions.py
@@ -0,0 +1,5 @@
+import enum
+
+class Directions(enum.Enum):
+	Down = 0
+	Up = 1
--- pdfMerger.py
+++ pdfMerger.py
@@ -3,6 +3,8 @@ from PyPDF2 import PdfFileMerger
 import tkinter as tk
 from tkinter import filedialog 
 from tkinter import messagebox
+from functools import partial
+from directions import Directions
 
 class Application(tk.Frame):
     listOfFilesToMerge = []
@@ -14,6 +16,8 @@ class Application(tk.Frame):
         self.create_widgets()
 
     #todo clean layout
+    #todo add buttons to change order of files
+    #todo allow drag and drop of files
     def create_widgets(self):
         #Listbox with all documents from listOfFiles
         self.listbox = tk.Listbox(self)
@@ -31,24 +35,42 @@ class Application(tk.Frame):
         self.mergeBtn["command"] = self.mergePdfs
         self.mergeBtn.pack(side="top")
 
+        #Button up arrow
+        self.upBtn = tk.Button(self)
+        self.upBtn["text"] = "↑"
+        self.upBtn["command"] = partial(self.moveElementInList, Directions.Up)
+        self.upBtn.pack(side="right")
+
+        #Button down arrow
+        self.downBtn = tk.Button(self)
+        self.downBtn["text"] ="↓"
+        self.downBtn["command"] = partial(self.moveElementInList, Directions.Down)
+        self.downBtn.pack(side="right")
+
         #quit button
         self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
         self.quit.pack(side="bottom")
 
+    #adds documents from fileselector to the listbox and listOfFilesToMerge
     def addDocumentToList(self):
         filenames = tk.filedialog.askopenfilenames(initialdir = ".", title = "Select a File", filetypes = (("PDF files", "*.pdf*"), ("all files", "*.*"))) 
         for file in filenames:
-            #todo shorter filename
-            self.listbox.insert("end", file)
             self.listOfFilesToMerge.append(file)
+        self.fillListboxFromList()
+
+    #update listbox with elements from listOfFilesToMerge
+    def fillListboxFromList(self):
+        #todo shorter filename
+        self.listbox.delete(0,tk.END)
+        for file in self.listOfFilesToMerge:
+            self.listbox.insert("end", file)
 
     def mergePdfs(self):
+         #TODO: disable btn if no files
         if not self.listOfFilesToMerge:
-            messagebox.showinfo("Title", "No pages to be merged") #TODO: disable btn if no files
+            messagebox.showinfo("Title", "No pages to be merged")
             return
-        
         resultfilename = tk.filedialog.asksaveasfilename(defaultextension=".pdf")
-
         pdfMerger = PdfFileMerger()
 
         #merge all pdfs together
@@ -60,6 +82,25 @@ class Application(tk.Frame):
             pdfMerger.write(fout)
 
         messagebox.showinfo("Title", "Merged successfully")
+
+    #change order of elements in list
+    #if dir is enum Direction.Up the currently selected item in the listbox switches with the upper elemet
+    #if dir is enum Direction.Down the selected item switches with the next element
+    def moveElementInList(self, dir):
+        picked = self.listbox.get(tk.ACTIVE)
+        pickedIdx = self.listOfFilesToMerge.index(picked)
+        if dir == Directions.Up:
+            #todo instead disable btn
+            if pickedIdx == 0:
+                return
+            self.listOfFilesToMerge[pickedIdx-1], self.listOfFilesToMerge[pickedIdx] = self.listOfFilesToMerge[pickedIdx], self.listOfFilesToMerge[pickedIdx-1]
+            self.fillListboxFromList()
+        elif dir == Directions.Down:
+            #todo instead disable btn
+            if pickedIdx == len(self.listOfFilesToMerge)-1:
+                return
+            self.listOfFilesToMerge[pickedIdx+1], self.listOfFilesToMerge[pickedIdx] = self.listOfFilesToMerge[pickedIdx], self.listOfFilesToMerge[pickedIdx+1]
+            self.fillListboxFromList()
 
 root = tk.Tk()
 root.geometry("500x300")
